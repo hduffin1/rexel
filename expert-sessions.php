03f745539998b51fb96470978868170920ddde91
Hayley Duffin <hduffin1@hotmail.com>
2021-01-30 12:41:49 +0000

    create banner template and replace on all page templates.

--- expert-sessions.php
+++ expert-sessions.php
@@ -2,21 +2,7 @@
 
 <?php get_header(); ?>
 
-<div class="banner-wrapper">
-    <div class="heading-wrapper">
-        <div class="container">
-            <h1>Mission <span>Possible</span></h1>
-
-            <div class="conf-banner">
-                <h2>Colleague Conference <span>2021</span></h2>
-            </div>
-
-            <div class="date-banner">
-                <h2>TUESDAY, 9 FEBRUARY  |  8.30AM – 3.45PM</h2>
-            </div>
-        </div>
-    </div>
-</div>
+<?php get_template_part('templates/banner'); ?>
 
 <div class="container">
     <div class="expert-session-holder">
--- login.php
+++ login.php
@@ -2,6 +2,8 @@
 
 <?php get_header(); ?>
 
+<?php get_template_part('templates/banner'); ?>
+
 <div class="login-wrapper">
 	<h3>Login Now</h3>
 	<?php echo do_shortcode("[wp_login_form]"); ?>
